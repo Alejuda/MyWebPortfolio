f6275be1d93a05432f9d5a25ab35ded2e91fb66b
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-04-10 16:29:59 -0300

    Fix linter errors

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const menuBtn = document.getElementById('nav-toggle-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 mobileMenu.classList.remove('show');
 
-menuBtn.addEventListener('click', function () {
-    mobileMenu.classList.toggle('show');
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('show');
 });
