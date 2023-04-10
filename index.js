e59062d9c6b5f3ae9ba7e4a4c9f1fe8326201f1b
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-04-10 15:13:44 -0400

    Add index.js file
    Added menuBtn functionality

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const menuBtn = document.getElementById('nav-toggle-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+mobileMenu.classList.remove('show');
 
+menuBtn.addEventListener('click', function () {
+    mobileMenu.classList.toggle('show');
+});
