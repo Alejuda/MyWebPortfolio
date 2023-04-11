02cff9425f942aeed2d9d646722bbf37ff62a685
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-04-11 16:53:48 -0400

    Update index.js file
    Added work objects to array works

--- index.js
+++ index.js
@@ -10,5 +10,74 @@ menuBtn.addEventListener("click", () => {
 
 let worksContainer = document.getElementById("works-container");
 
-const works = [];
+const works = [
+    {
+        id: 1,
+        title: "Data Dashboard Healthcare",
+        content:
+          "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        habilities: ["HTML", "Bootstrap", "Ruby"],
+        img: "./assets/icons/Img Placeholder-2-desktop.png",
+        liveLink: "#",
+        sourceLink: "#",
+      },
+      {
+        id: 2,
+        title: "Website Portfolio",
+        content:
+          "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        habilities: ["HTML", "Bootstrap", "Ruby"],
+        img: "./assets/icons/Img Placeholder-3-desktop.png",
+        liveLink: "#",
+        sourceLink: "#",
+      },
+      {
+        id: 3,
+        title: "Profesional Art Printing Data More",
+        content:
+          "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        habilities: ["HTML", "Bootstrap", "Ruby"],
+        img: "./assets/img/Img Placeholder2.png",
+        liveLink: "#",
+        sourceLink: "#",
+      },
+      {
+        id: 4,
+        title: "Data Dashboard Healthcare",
+        content:
+          "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        habilities: ["HTML", "Bootstrap", "Ruby"],
+        img: "./assets/icons/Img Placeholder-2-desktop.png",
+        liveLink: "#",
+        sourceLink: "#",
+      },
+      {
+        id: 5,
+        title: "Website Portfolio",
+        content:
+          "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        habilities: ["HTML", "Bootstrap", "Ruby"],
+        img: "./assets/icons/Img Placeholder-3-desktop.png",
+        liveLink: "#",
+        sourceLink: "#",
+      },
+      {
+        id: 6,
+        title: "Multi-Post stories",
+        content: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        habilities: ["CSS", "HTML", "Bootstrap", "Ruby"],
+        img: "./assets/img/Img\ Placeholder.png",
+        liveLink: "#",
+        sourceLink: "#",
+      },
+      {
+        id: 7,
+        title: "Profesional Art Printing Data More",
+        content: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+        habilities: ["CSS", "HTML", "Ruby"],
+        img: "./assets/icons/Img\ Placeholder-1-desktop.png",
+        liveLink: "#",
+        sourceLink: "#",
+      },
+];
 
